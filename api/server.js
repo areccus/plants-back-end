1b7b9c63474adf78ee6ee51a1eafa8ca77cd887c
Areccus Branch <areccusbranch@Areccuss-MacBook.local>
2022-02-02 17:03:33 -0500

    5:03

--- api/server.js
+++ api/server.js
@@ -13,10 +13,6 @@ server.use(express.json());
 server.use('/api/auth', authRouter)
 server.use('/api/plants', plantsRouter)
 
-server.get('/testing', (req, res) => {
-    res.json({message: 'testing message'})
-})
-
 server.use((err, req, res, next) => { 
     res.status(err.status || 500).json({
       message: err.message,
