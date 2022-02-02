92cb84090f63881c0c95fa81e81d91bf37bddd3b
Areccus Branch <areccusbranch@Areccuss-MacBook.local>
2022-02-02 17:31:19 -0500

    5:31

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-require('dotenv').config()
 const server = require('./api/server');
 
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 9000;
 server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
