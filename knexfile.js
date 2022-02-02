40ccd2558fe7d6241a83183b72273133ccb966a5
Areccus Branch <areccusbranch@Areccuss-MacBook.local>
2022-02-02 16:32:20 -0500

    4:32

--- knexfile.js
+++ knexfile.js
@@ -1,30 +1,28 @@
+require('dotenv').config()
+const pg = require('pg')
+
+if (process.env.DATABASE_URL) {
+  pg.defaults.ssl = { rejectUnauthorized: false }
+}
+
 const sharedConfig = {
-    client: 'sqlite3',
-    useNullAsDefault: true,
-    migrations: {
-      directory: './data/migrations',
-    },
-    seeds: {
-      directory: './data/seeds',
-    },
-    pool: {
-      afterCreate: (conn, done) => {
-        conn.run('PRAGMA foreign_keys = ON', done)
-      },
-    },
-    production: {
-      client: 'sqlite3',
-      connection: process.env.DATABASE_URL,
-      migrations: {
-          directory: './data/migrations',
-      },
-      seeds: { directory: './data/seeds' },
+  client: 'pg',
+  migrations: { directory: './data/migrations' },
+  seeds: { directory: './data/seeds' },
+}
+
+module.exports = {
+  development: {
+    ...sharedConfig,
+    connection: process.env.DEV_DATABASE_URL,
   },
-  }
-  module.exports = {
-    development: {
-      ...sharedConfig,
-      connection: { filename: './data/plants.db3' },
-    }
-  }
-  
+  testing: {
+    ...sharedConfig,
+    connection: process.env.TESTING_DATABASE_URL,
+  },
+  production: {
+    ...sharedConfig,
+    connection: process.env.DATABASE_URL,
+    pool: { min: 2, max: 10 },
+  },
+}
