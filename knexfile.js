06b0dd6aa462b8b5abee00b480fb64596020fe4d
Areccus Branch <areccusbranch@Areccuss-MacBook.local>
2022-02-02 20:40:28 -0500

    8:40

--- knexfile.js
+++ knexfile.js
@@ -1,53 +1,58 @@
-// const sharedConfig = {
-//     client: 'sqlite3',
-//     useNullAsDefault: true,
-//     migrations: {
-//       directory: './data/migrations',
-//     },
-//     seeds: {
-//       directory: './data/seeds',
-//     },
-//     pool: {
-//       afterCreate: (conn, done) => {
-//         conn.run('PRAGMA foreign_keys = ON', done)
-//       },
-//     },
-//   }
-//   module.exports = {
-//     development: {
-//       ...sharedConfig,
-//       connection: { filename: './data/plants.db3' },
-//     },
-//     production: {
-//       ...sharedConfig,
-//       connection: { filename: './data/plants.db3' },
-//       pool: { min: 2, max: 10 },
-//     },
-//   }
+const sharedConfig = {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done)
+      },
+    },
+  }
+  module.exports = {
+    development: {
+      ...sharedConfig,
+      connection: { filename: './data/plants.db3' },
+    },
+    production: {
+      ...sharedConfig,
+      connection: { filename: './data/plants.db3' },
+      pool: { min: 2, max: 10 },
+    },
+  }
 
-require('dotenv').config()
+// require('dotenv').config()
+
 // const pg = require('pg')
 
-if (process.env.DATABASE_URL) {
-  pg.defaults.ssl = { rejectUnauthorized: false }
-}
+// if (process.env.DATABASE_URL) {
+//   pg.defaults.ssl = { rejectUnauthorized: false }
+// }
 
-const sharedConfig = {
-  client: 'sqlite3',
-  migrations: { directory: './api/data/migrations' },
-  seeds: { directory: './api/data/seeds' },
-}
+// const sharedConfig = {
+//   client: 'pg',
+//   migrations: { directory: './api/data/migrations' },
+//   seeds: { directory: './api/data/seeds' },
+// }
 
-module.exports = {
-  development: {
-    ...sharedConfig,
-    connection: { filename: './data/plants.db3' },
-  },
-  production: {
-    ...sharedConfig,
-    connection: process.env.DATABASE_URL,
-    pool: { min: 2, max: 10 },
-  },
-}
+// module.exports = {
+//   development: {
+//     ...sharedConfig,
+//     connection: process.env.DEV_DATABASE_URL,
+//   },
+//   testing: {
+//     ...sharedConfig,
+//     connection: process.env.TESTING_DATABASE_URL,
+//   },
+//   production: {
+//     ...sharedConfig,
+//     connection: process.env.DATABASE_URL,
+//     pool: { min: 2, max: 10 },
+//   },
+// }
 
   
